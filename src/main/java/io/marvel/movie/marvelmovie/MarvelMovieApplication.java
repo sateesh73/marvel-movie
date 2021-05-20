b0d6749611e4d1c3e949eb582300f0ae4ea8d7fd
sateesh73 <sateeshvarma73@gmail.com>
2021-05-20 15:52:59 +0530

    initalizin repo

---
+++ src/main/java/io/marvel/movie/marvelmovie/MarvelMovieApplication.java
@@ -0,0 +1,13 @@
+package io.marvel.movie.marvelmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/marvel/movie/marvelmovie/MarvelMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package io.marvel.movie.marvelmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
